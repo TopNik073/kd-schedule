32c8ded9cba04d853840c6322f680504fdff504c
TopNik073 <borzovn169@gmail.com>
2025-04-17 12:50:51 +0200

    Create openapi generation scripts

---
+++ scripts/generate_openapi.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ.setdefault("DB_USER", "db_user")
+os.environ.setdefault("DB_PASS", "db_pass")
+os.environ.setdefault("DB_HOST", "db_host")
+os.environ.setdefault("DB_PORT", "5432")
+os.environ.setdefault("DB_NAME", "db_name")
+
+from src.main import app
+import json
+import yaml
+
+
+def generate_openapi_spec(path: str = "../docs/", name: str = "openapi", ext: str = "yaml"):
+    """
+    Generate an OpenAPI specification file.
+    """
+    os.makedirs(path, exist_ok=True)
+
+    openapi_spec = app.openapi()
+    with open(f"{path}{name}.{ext}", "w") as f:
+        if ext == "json":
+            json.dump(openapi_spec, f)
+        elif ext == "yaml":
+            yaml.dump(openapi_spec, f)
+        else:
+            raise ValueError(f"Unsupported file extension: {ext}")
+
+
+if __name__ == "__main__":
+    generate_openapi_spec(ext="yaml")
+    generate_openapi_spec(ext="json")
