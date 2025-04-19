0faa64033f8ed73296810dcdb1c161d6cc78f840
TopNik073 <borzovn169@gmail.com>
2025-04-19 21:56:12 +0200

    feat(tests): create tests for gRPC server
    It's better to run server and tests separately, because when gRPC running with fixture, db connections will not be closed (but tests pass)

---
+++ tests/test_schedule_servicer.py
@@ -0,0 +1,108 @@
+import pytest
+import grpc
+import uuid
+from datetime import timedelta
+
+from src.grpc.schedule_pb2_grpc import ScheduleServiceStub
+
+from src.grpc.schedule_pb2 import (
+    CreateScheduleRequest,
+    GetSchedulesIdsRequest,
+    GetScheduleRequest,
+    GetNextTakingsRequest,
+)
+from tests.models import UserTest, MedicineTest
+
+
+@pytest.mark.asyncio(loop_scope="session")
+class TestScheduleServicer:
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("use_end_time", [True, False])
+    async def test_schedule_servicer_e2e(self, get_test_user, get_test_medicine, use_end_time):
+        channel = grpc.aio.insecure_channel("localhost:50051")
+        stub = ScheduleServiceStub(channel)
+
+        # Step 1: Create a schedule
+        schedule_id = await self._create_schedule(
+            stub, get_test_user, get_test_medicine, use_end_time
+        )
+
+        assert schedule_id is not None
+        assert uuid.UUID(schedule_id)
+
+        # Step 2: Get the schedules ids
+        schedules_ids = await self._get_schedules_ids(stub, get_test_user)
+
+        assert len(schedules_ids.schedule_ids) >= 1
+        assert schedule_id in schedules_ids.schedule_ids
+
+        # # Step 3: Get the schedule
+        response = await self._get_schedule(stub, schedule_id, get_test_user)
+
+        assert hasattr(response, "schedule")
+        schedule = response.schedule
+        assert schedule is not None
+        assert schedule.medicine_name == get_test_medicine.medicine_name
+        assert schedule.frequency == get_test_medicine.frequency
+        assert schedule.end_date is not None
+
+        # Step 4: Get the next takings
+        next_takings = await self._get_next_takings(stub, get_test_user)
+
+        assert next_takings is not None
+        assert len(next_takings.takings) >= 1
+
+        next_taking = [
+            next_taking
+            for next_taking in next_takings.takings
+            if next_taking.schedule_info.medicine_name == get_test_medicine.medicine_name
+        ][0]
+
+        assert next_taking is not None
+
+        await channel.close()
+
+    async def _send_request(self, method: grpc.aio._channel.UnaryUnaryMultiCallable, request):
+        try:
+            response = await method(request)
+            print(response)
+            assert response is not None
+            return response
+        except grpc.RpcError as e:
+            pytest.fail(f"Error sending request {method.__repr__}: {e}")
+        except Exception as e:
+            pytest.fail(f"Error sending request {method.__repr__}: {e}")
+
+    async def _create_schedule(
+        self, stub, test_user: UserTest, test_medicine: MedicineTest, use_end_time: bool
+    ):
+        request = CreateScheduleRequest(
+            name=test_user.name,
+            medicine_policy=test_user.medicine_policy,
+            medicine_name=test_medicine.medicine_name,
+            frequency=test_medicine.frequency,
+            start_date=test_medicine.start_date,
+        )
+
+        if use_end_time:
+            request.end_date.FromDatetime(test_medicine.end_date)
+        else:
+            request.duration.FromTimedelta(test_medicine.duration)
+
+        response = await self._send_request(stub.CreateSchedule, request)
+        return response.id
+
+    async def _get_schedules_ids(self, stub, test_user: UserTest):
+        request = GetSchedulesIdsRequest(user_id=test_user.medicine_policy)
+        return await self._send_request(stub.GetSchedulesIds, request)
+
+    async def _get_schedule(self, stub, schedule_id: uuid.UUID, test_user: UserTest):
+        request = GetScheduleRequest(
+            schedule_id=str(schedule_id), user_id=test_user.medicine_policy
+        )
+        return await self._send_request(stub.GetSchedule, request)
+
+    async def _get_next_takings(self, stub, test_user: UserTest):
+        request = GetNextTakingsRequest(user_id=test_user.medicine_policy)
+        request.next_takings_interval.FromTimedelta(timedelta(hours=2))
+        return await self._send_request(stub.GetNextTakings, request)
