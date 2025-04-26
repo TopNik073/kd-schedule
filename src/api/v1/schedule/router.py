7e4a186c7a2e73590297228be125437ea8f08c5e
TopNik073 <borzovn169@gmail.com>
2025-04-27 00:18:28 +0200

    Remove logs from router

--- src/api/v1/schedule/router.py
+++ src/api/v1/schedule/router.py
@@ -1,8 +1,7 @@
 from datetime import timedelta
 from typing import TYPE_CHECKING
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, Query
 
-from src.core.logging import get_logger
 from src.core.config import settings
 
 from .dependencies import get_schedule_service
@@ -22,8 +21,6 @@ from uuid import UUID
 if TYPE_CHECKING:
     from src.api.v1.schedule.service import ScheduleService
 
-logger = get_logger(__name__)
-
 router = APIRouter(tags=["schedule"])
 
 
@@ -38,15 +35,8 @@ async def create_schedule(
     If only duration is provided, it will use current time + duration.
     If start_date is provided, it will use start_date.
     """
-    try:
-        schedule_id = await schedule_service.create_schedule(create_schedule_dto)
-        return SuccessResponseSScheduleCreateResponse(data=SScheduleCreateResponse(id=schedule_id))
-    except ValueError as e:
-        logger.exception(f"Validation error", exc_info=e)
-        raise HTTPException(status_code=400, detail=str(e))
-    except Exception as e:
-        logger.exception(f"Error creating schedule", exc_info=e)
-        raise HTTPException(status_code=500, detail="Internal server error")
+    schedule_id = await schedule_service.create_schedule(create_schedule_dto)
+    return SuccessResponseSScheduleCreateResponse(data=SScheduleCreateResponse(id=schedule_id))
 
 
 @router.get("/schedules")
@@ -57,17 +47,8 @@ async def get_schedules_ids(
     """
     Get all schedules ids for a user.
     """
-    try:
-        schedule_ids = await schedule_service.get_schedules_ids_by_policy(user_id)
-        return SuccessResponseListUUID(data=schedule_ids)
-    except ValueError as e:
-        logger.exception(f"Validation error", exc_info=e)
-        raise HTTPException(status_code=400, detail=str(e))
-    except HTTPException:
-        raise
-    except Exception as e:
-        logger.exception(f"Error getting schedules", exc_info=e)
-        raise HTTPException(status_code=500, detail="Internal server error")
+    schedule_ids = await schedule_service.get_schedules_ids_by_policy(user_id)
+    return SuccessResponseListUUID(data=schedule_ids)
 
 
 @router.get("/schedule")
@@ -79,24 +60,15 @@ async def get_schedule(
     """
     Get a schedule by id.
     """
-    try:
-        schedule = await schedule_service.get_schedule_by_id(user_id, schedule_id)
-        return SuccessResponseSGetScheduleResponse(
-            data=SGetScheduleResponse(
-                medicine_name=schedule.medicine_name,
-                frequency=schedule.frequency,
-                start_date=schedule.start_date,
-                end_date=schedule.end_date,
-            )
+    schedule = await schedule_service.get_schedule_by_id(user_id, schedule_id)
+    return SuccessResponseSGetScheduleResponse(
+        data=SGetScheduleResponse(
+            medicine_name=schedule.medicine_name,
+            frequency=schedule.frequency,
+            start_date=schedule.start_date,
+            end_date=schedule.end_date,
         )
-    except ValueError as e:
-        logger.exception(f"Validation error", exc_info=e)
-        raise HTTPException(status_code=400, detail=str(e))
-    except HTTPException:
-        raise
-    except Exception as e:
-        logger.exception(f"Error getting schedule by id", exc_info=e)
-        raise HTTPException(status_code=500, detail="Internal server error")
+    )
 
 
 @router.get("/next_takings")
@@ -111,16 +83,7 @@ async def get_next_takings(
     Get next takings for a user.
     If next_takings is provided, it will use it instead of the default value from the config.
     """
-    try:
-        takings: list[SGetNextTakingsResponse] = await schedule_service.get_next_takings_with_model(
-            user_id, next_takings
-        )
-        return SuccessResponseListSGetNextTakingsResponse(data=takings)
-    except ValueError as e:
-        logger.exception(f"Validation error", exc_info=e)
-        raise HTTPException(status_code=400, detail=str(e))
-    except HTTPException:
-        raise
-    except Exception as e:
-        logger.exception(f"Error getting taking schedule", exc_info=e)
-        raise HTTPException(status_code=500, detail="Internal server error")
+    takings: list[SGetNextTakingsResponse] = await schedule_service.get_next_takings_with_model(
+        user_id, next_takings
+    )
+    return SuccessResponseListSGetNextTakingsResponse(data=takings)
